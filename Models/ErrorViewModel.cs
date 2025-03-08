c080d4757cbeda3062734645f2e5e54e43c3a75b
mukesh100592 <mukesh100592@gmail.com>
2025-03-08 14:11:57 +0530

    RailwayManagementSystem project startup files added.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RailwayManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
